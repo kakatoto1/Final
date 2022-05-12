ed17b6a1f5d28c3acf3c46161d098590f6777ead
kakatoto1 <98189795+kakatoto1@users.noreply.github.com>
2022-05-12 17:07:30 -0400

    test_config_test
    import logging
    import os
    
    import app.config
    
    def test_testing_config(application):
        application.config.from_object('app.config.TestingConfig')
        assert application.config['DEBUG']
        assert application.config['TESTING']
        assert not application.config['PRESERVE_CONTEXT_ON_EXCEPTION']
        assert application.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:///:memory:'

---
+++ tests/test_config_test.py
@@ -0,0 +1,12 @@
+
+import logging
+import os
+
+import app.config
+
+def test_testing_config(application):
+    application.config.from_object('app.config.TestingConfig')
+    assert application.config['DEBUG']
+    assert application.config['TESTING']
+    assert not application.config['PRESERVE_CONTEXT_ON_EXCEPTION']
+    assert application.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:///:memory:'
