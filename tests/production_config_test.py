e2cb5b4b07e9c45b2e3128c4fc38588b10fc69e2
kakatoto1 <98189795+kakatoto1@users.noreply.github.com>
2022-05-12 17:04:17 -0400

    production_config_test
    import logging
    import os
    
    import app.config
    def test_production_config(application):
        application.config.from_object('app.config.ProductionConfig')
        assert not application.config['DEBUG']
        assert not application.config['TESTING']

---
+++ tests/production_config_test.py
@@ -0,0 +1,11 @@
+
+import logging
+import os
+
+import app.config
+
+
+def test_production_config(application):
+    application.config.from_object('app.config.ProductionConfig')
+    assert not application.config['DEBUG']
+    assert not application.config['TESTING']
