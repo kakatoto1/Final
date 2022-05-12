bf7827ba2837b2ed665ab4689c2cfdcd4dcbfe5a
kakatoto1 <98189795+kakatoto1@users.noreply.github.com>
2022-05-12 17:05:46 -0400

    develop config test
    import logging
    import os
    
    import app.config
    
    def test_development_config(application):
        application.config.from_object('app.config.DevelopmentConfig')
    
        assert application.config['DEBUG']
        assert not application.config['TESTING']

---
+++ tests/Dev_config_test.py
@@ -0,0 +1,12 @@
+
+import logging
+import os
+
+import app.config
+
+def test_development_config(application):
+    application.config.from_object('app.config.DevelopmentConfig')
+
+    assert application.config['DEBUG']
+    assert not application.config['TESTING']
+
