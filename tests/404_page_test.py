0a5b50f19b1f945ee65247accf9e443afe0b9181
kakatoto1 <98189795+kakatoto1@users.noreply.github.com>
2022-05-12 17:36:38 -0400

    404_page_test
    def test_request_page_not_found(client):
        response = client.get("/page5")
        assert response.status_code == 404

--- tests/404_page_test.py
+++ tests/404_page_test.py
@@ -1,3 +1,11 @@
+def test_request_main_menu_links(client):
+    """This makes the index page"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b'href="/about"' in response.data
+    assert b'href="/welcome"' in response.data
+    assert b'href="/login"' in response.data
+    assert b'href="/register"' in response.data
 
 def test_request_page_not_found(client):
     response = client.get("/page5")
