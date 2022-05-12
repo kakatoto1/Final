534191167e120bc2ad97f7170140fd9490eee325
kakatoto1 <98189795+kakatoto1@users.noreply.github.com>
2022-05-12 16:54:29 -0400

    Testing the Calculator from calculator import Calculator
    def test_calculator_is_instance():
        calculator = Calculator()
        assert isinstance(calculator, Calculator)

---
+++ tests/calculator_test_instance.py
@@ -0,0 +1,11 @@
+"""Testing the Calculator"""
+from calculator import Calculator
+
+
+def test_calculator_is_instance():
+    """Testing the Calculator"""
+    calculator = Calculator()
+    assert isinstance(calculator, Calculator)
+
+
+
